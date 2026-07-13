f2ca5bba659fb542881315a17a393bf76cc68a3c
Lalitforreal <S24CSEU0093@bennett.edu.in>
2026-07-13 15:29:31 +0530

    refactor

--- room-practice-ts/src/index.ts
+++ room-practice-ts/src/index.ts
@@ -4,26 +4,37 @@ import {Server} from "socket.io";
 import http from "http";
 import path from "path";
 import type { Request, Response } from "express";
-import type { Socket } from "dgram";
 
+const server = http.createServer(app);
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerData, SocketData>(server);
 
+// Socket.IO has no idea what events exist in your app. So when you write socket.emit("welcome", ...)
+// — TypeScript can't help you. Wrong event name, wrong payload shape, wrong argument count — 
+// all silently allowed. You only find out something's wrong at runtime when nothing works.
+// You're telling Socket.IO: "here is the exact contract of every event in this app.
+// " Now the Server class internally uses those interfaces to type every socket it creates. So when you write socket.emit(...)
+//  inside io.on("connection", socket => {...}) — TypeScript already knows socket can only emit ServerToClientEvents,
+//   and can only receive ClientToServerEvents.
+// Wrong event name → red line immediately. Wrong payload shape → red line immediately. Before you even run the server.
 
 
 interface ClientToServerEvents{
     "room-join" : (roomId : string) => void;
     "send-to-room" : ( data : { msg : string}) =>void;
 }
+
 interface ServerToClientEvents{
-    "welcome" : (msg : string) => void;
-    "send-to-room" : (data : {msg : string}) => void;
+    "welcome" : (data : {msg : string, from : string}) => void;
+    "send-to-room" : (data : {msg : string,
+        from : string
+    }) => void;
 }
+
 interface InterServerData{ };
 
 interface SocketData{
     roomId : string
 }
-const server = http.createServer(app);
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerData, SocketData>(server);
 
 
 app.get('/', (req : Request, res : Response)=>{
@@ -38,15 +49,27 @@ const __dirname = path.dirname(__filename);
 app.get('/client1', (req : Request, res : Response)=>{
     res.sendFile(path.join(__dirname, "public","client.html"));
 })
+
 io.on("connection",(socket)=>{
     console.log("connected hai ji socket" , socket.id);
+
     
     socket.on("room-join", (roomId)=>{
         socket.data.roomId = roomId;
         socket.join(roomId);
         console.log(socket.data.roomId);
-        socket.emit("welcome", "welcome ji " + socket.id);
+
+        socket.emit("welcome",{
+             msg : "hey ji welcome",
+             from : socket.id
+            }
+        );
+
     })
+    socket.on("send-to-room", (data)=>{
+        io.to(socket.data.roomId).emit("send-to-room", {msg: data.msg , from : socket.id});
+    })
+    
 })
  
 server.listen(3000, ()=>{
